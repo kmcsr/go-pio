61dc3b46c188dd3174ab2ddd5f1368f5b6e9429d
zyxkad <zyxkad@gmail.com>
2022-09-30 15:42:21 -0600

    new type `Ok` for var `OkPkt`

--- internal.go
+++ internal.go
@@ -12,14 +12,16 @@ type (
 	Pong struct{
 		Payload uint64
 	}
+	Ok struct{}
 )
 
 var _ PacketAsk = (*Ping)(nil)
 var _ PacketBase = (*Pong)(nil)
-var OkPkt PacketBase = NewPkt(0x04)
+var OkPkt PacketBase = Ok{}
 
 func (*Ping)PktId()(uint32){ return 0x01 }
 func (*Pong)PktId()(uint32){ return 0x02 }
+func (Ok)PktId()(uint32){ return 0x04 }
 
 func (p *Ping)ParseFrom(r encoding.Reader)(err error){
 	p.Payload, err = r.ReadUint64()
@@ -48,6 +50,8 @@ func (p *Pong)WriteTo(w encoding.Writer)(err error){
 	return
 }
 
+func (Ok)WriteTo(encoding.Writer)(error){ return nil }
+func (Ok)ParseFrom(encoding.Reader)(error){ return nil }
 
 type streamingError struct{}
 
